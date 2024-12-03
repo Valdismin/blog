51b3d8326e2a4a0f427280b7627ea23dedad7e22
Vladislav Kozlovski <Valdismin@gmail.com>
2024-12-03 22:02:33 +0300

    Updates

---
+++ navbar-mock.ts
@@ -0,0 +1,86 @@
+export const navigationItems = [
+    {
+        title: 'Technology',
+        iconLink: '/icons/bulb.svg',
+        details: [
+            {
+                detailTitle: 'Robotics',
+                link:'/feed/robotics'
+            },
+            {
+                detailTitle: 'Cyber Security',
+                link:''
+            },
+            {
+                detailTitle: 'Databases',
+                link:''
+            },
+            {
+                detailTitle: 'Electronics',
+                link:''
+            },
+        ]
+    },
+    {
+        title: 'Movies',
+        iconLink: '/icons/movie.svg',
+        details: [
+            {
+                detailTitle: 'Hollywood',
+                link:''
+            },
+            {
+                detailTitle: 'Web Series',
+                link:''
+            },
+            {
+                detailTitle: 'Romance',
+                link:''
+            },
+            {
+                detailTitle: 'Horror',
+                link: ''
+            },
+        ]
+    },
+    {
+        title: 'Sports',
+        iconLink: '/icons/sports.svg',
+        details: [
+            {
+                detailTitle: 'Soccer',
+                link: ''
+            },
+            {
+                detailTitle: 'Cricket',
+                link: ''
+            },
+            {
+                detailTitle: 'Volleyball',
+                link: ''
+            },
+            {
+                detailTitle: 'Tennis',
+                link: ''
+            },
+        ]
+    },
+    {
+        title: 'Politics',
+        iconLink: '/icons/politics.svg',
+        details: [
+            {
+                detailTitle: 'Election',
+                link: ''
+            },
+            {
+                detailTitle: 'Political Party',
+                link: ''
+            },
+            {
+                detailTitle: 'Politicians',
+                link: ''
+            }
+        ]
+    },
+]
